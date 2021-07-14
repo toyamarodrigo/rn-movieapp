1715f0e0713b38fabf27cd05e1047083e6307dba
Toyama Rodrigo <toyama.rodrigo@gmail.com>
2021-07-14 00:15:05 -0300

    Carousel Multi styled, genre movies

--- src/components/CarouselMulti.js
+++ src/components/CarouselMulti.js
@@ -12,6 +12,9 @@ export default function CarouselMulti({ data, navigation }) {
   return (
     <Carousel
       data={data}
+      firstItem={1}
+      inactiveSlideOpacity={1}
+      inactiveSlideScale={1}
       itemWidth={ITEM_WIDTH}
       layout="default"
       renderItem={(item) => <RenderItem data={item} navigation={navigation} />}
@@ -20,10 +23,49 @@ export default function CarouselMulti({ data, navigation }) {
   );
 }
 
-function RenderItem() {
+function RenderItem({
+  data,
+  data: {
+    item: { id, title, poster_path },
+  },
+  navigation,
+}) {
+  const imageURL = `${BASE_PATH_IMG}/w500${poster_path}`;
+
+  const onNavigation = () => {
+    navigation.navigate(id);
+  };
+
   return (
-    <View>
-      <Title>Hola</Title>
-    </View>
+    <TouchableWithoutFeedback onPress={onNavigation}>
+      <View style={styles.card}>
+        <Image source={{ uri: imageURL }} style={styles.image} />
+        <Title numberOfLines={1} style={styles.title}>
+          {title}
+        </Title>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
+
+const styles = StyleSheet.create({
+  card: {
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 10,
+      shadowOpacity: 1,
+      shadowRadius: 10,
+    },
+  },
+  image: {
+    width: "85%",
+    height: 170,
+    borderRadius: 10,
+  },
+  title: {
+    marginHorizontal: 10,
+    marginTop: 10,
+    fontSize: 16,
+  },
+});
