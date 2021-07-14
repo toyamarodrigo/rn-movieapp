99eed26b043e90490ecfda296d681d658b865b0b
Toyama Rodrigo <toyama.rodrigo@gmail.com>
2021-07-14 19:45:09 -0300

    Popular movies styling

--- src/screens/Popular/Popular.js
+++ src/screens/Popular/Popular.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { ScrollView, StyleSheet, View, Image } from "react-native";
+import { Text } from "react-native-paper";
+import { map } from "lodash";
 
+import { BASE_PATH_IMG } from "../../utils/constants";
+import noImage from "../../assets/png/default-image.png";
 import { getPopularMoviesApi } from "../../api/movies";
 
 export const Popular = ({ navigation }) => {
@@ -15,10 +19,41 @@ export const Popular = ({ navigation }) => {
   }, []);
 
   return (
-    <View>
-      <Text>POPULAR SCREEN</Text>
-    </View>
+    <ScrollView showsVerticalScrollIndicator={false}>
+      {map(movies, (movie, index) => (
+        <Movie key={index} movie={movie} />
+      ))}
+    </ScrollView>
   );
 };
 
-const styles = StyleSheet.create({});
+function Movie({ movie: { poster_path } }) {
+  return (
+    <View style={styles.movie}>
+      <View style={styles.left}>
+        <Image
+          source={poster_path ? { uri: `${BASE_PATH_IMG}/w500${poster_path}` } : { noImage }}
+          style={styles.image}
+        />
+      </View>
+      <View style={styles.right}>
+        <Text>RIGHT</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  movie: {
+    marginBottom: 20,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  left: {
+    marginRight: 20,
+  },
+  image: {
+    width: 100,
+    height: 150,
+  },
+});
