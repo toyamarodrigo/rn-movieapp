7d20770a872b7b0277e59614375c1d9ea8bd7b6f
Toyama Rodrigo <toyama.rodrigo@gmail.com>
2021-07-14 21:16:58 -0300

    New movies screen

--- src/screens/News/News.js
+++ src/screens/News/News.js
@@ -1,12 +1,106 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import {
+  StyleSheet,
+  View,
+  ScrollView,
+  TouchableWithoutFeedback,
+  Image,
+  Dimensions,
+} from "react-native";
+import { Button, Text } from "react-native-paper";
+import { map } from "lodash";
 
-export const News = () => {
+import userPreferences from "../../hooks/userPreferences";
+import { BASE_PATH_IMG } from "../../utils/constants";
+import { getNewsMoviesApi } from "../../api/movies";
+
+const { width } = Dimensions.get("window");
+
+export const News = ({ navigation }) => {
+  const [movies, setMovies] = useState(null);
+  const [page, setPage] = useState(1);
+  const [showButtonMore, setShowButtonMore] = useState(true);
+
+  const { theme } = userPreferences();
+
+  useEffect(() => {
+    (async () => {
+      const data = await getNewsMoviesApi(page);
+      const totalPages = data.total_pages;
+
+      if (page < totalPages) {
+        if (!movies) {
+          setMovies(data.results);
+        } else {
+          setMovies([...movies, ...data.results]);
+        }
+      } else {
+        setShowButtonMore(false);
+      }
+    })();
+  }, [page]);
+
   return (
-    <View>
-      <Text>NEWS SCREEN</Text>
-    </View>
+    <ScrollView showsVerticalScrollIndicator={false}>
+      <View style={styles.container}>
+        {map(movies, (movie, index) => (
+          <Movie key={index} movie={movie} navigation={navigation} />
+        ))}
+      </View>
+      {showButtonMore && (
+        <Button
+          contentStyle={styles.loadMoreContainer}
+          labelStyle={{ color: theme === "dark" ? "#fff" : "#000" }}
+          model="container"
+          style={styles.loadMore}
+          onPress={() => setPage(page + 1)}
+        >
+          Show More...
+        </Button>
+      )}
+    </ScrollView>
   );
 };
 
-const styles = StyleSheet.create({});
+function Movie({ movie: { id, title, poster_path }, navigation }) {
+  const goMovie = () => {
+    navigation.navigate("movie", { id });
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={goMovie}>
+      <View style={styles.movie}>
+        {poster_path ? (
+          <Image source={{ uri: `${BASE_PATH_IMG}/w500${poster_path}` }} style={styles.image} />
+        ) : (
+          <Text>{title}</Text>
+        )}
+      </View>
+    </TouchableWithoutFeedback>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  movie: {
+    width: width / 3,
+    height: 200,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+  },
+  loadMoreContainer: {
+    paddingTop: 10,
+    paddingBottom: 30,
+  },
+  loadMore: {
+    backgroundColor: "transparent",
+  },
+});
--- src/screens/Popular/Popular.js
+++ src/screens/Popular/Popular.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { ScrollView, StyleSheet, View, Image } from "react-native";
-import { Title, Text } from "react-native-paper";
+import { ScrollView, StyleSheet, View, Image, TouchableWithoutFeedback } from "react-native";
+import { Title, Text, Button } from "react-native-paper";
 import { map } from "lodash";
 import { Rating } from "react-native-ratings";
 
@@ -13,40 +13,72 @@ import { getPopularMoviesApi } from "../../api/movies";
 
 export const Popular = ({ navigation }) => {
   const [movies, setMovies] = useState(null);
+  const [showButtonMore, setShowButtonMore] = useState(true);
+  const [page, setPage] = useState(1);
   const { theme } = userPreferences();
 
   useEffect(() => {
     (async () => {
-      const data = await getPopularMoviesApi(1);
+      const data = await getPopularMoviesApi(page);
+      const totalPages = data.total_pages;
 
-      setMovies(data.results);
+      if (page < totalPages) {
+        if (!movies) {
+          setMovies(data.results);
+        } else {
+          setMovies([...movies, ...data.results]);
+        }
+      } else {
+        setShowButtonMore(false);
+      }
     })();
-  }, []);
+  }, [page]);
 
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
       {map(movies, (movie, index) => (
-        <Movie key={index} movie={movie} theme={theme} />
+        <Movie key={index} movie={movie} navigation={navigation} theme={theme} />
       ))}
+      {showButtonMore && (
+        <Button
+          contentStyle={styles.loadMoreContainer}
+          labelStyle={{ color: theme === "dark" ? "#fff" : "#000" }}
+          mode="contained"
+          style={styles.loadMore}
+          onPress={() => setPage(page + 1)}
+        >
+          Show More...
+        </Button>
+      )}
     </ScrollView>
   );
 };
 
-function Movie({ theme, movie: { poster_path, title, release_date, vote_count, vote_average } }) {
+function Movie({
+  navigation,
+  theme,
+  movie: { poster_path, title, release_date, vote_count, vote_average, id },
+}) {
+  const goMovie = () => {
+    navigation.navigate("movie", { id });
+  };
+
   return (
-    <View style={styles.movie}>
-      <View style={styles.left}>
-        <Image
-          source={poster_path ? { uri: `${BASE_PATH_IMG}/w500${poster_path}` } : { noImage }}
-          style={styles.image}
-        />
+    <TouchableWithoutFeedback onPress={goMovie}>
+      <View style={styles.movie}>
+        <View style={styles.left}>
+          <Image
+            source={poster_path ? { uri: `${BASE_PATH_IMG}/w500${poster_path}` } : { noImage }}
+            style={styles.image}
+          />
+        </View>
+        <View style={styles.right}>
+          <Title>{title}</Title>
+          <Text>{release_date}</Text>
+          <MovieRating theme={theme} voteAverage={vote_average} voteCount={vote_count} />
+        </View>
       </View>
-      <View style={styles.right}>
-        <Title>{title}</Title>
-        <Text>{release_date}</Text>
-        <MovieRating theme={theme} voteAverage={vote_average} voteCount={vote_count} />
-      </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 }
 
@@ -86,5 +118,12 @@ const styles = StyleSheet.create({
     alignItems: "flex-start",
     justifyContent: "flex-start",
     marginTop: 10,
+  },
+  loadMoreContainer: {
+    paddingTop: 10,
+    paddingBottom: 30,
+  },
+  loadMore: {
+    backgroundColor: "transparent",
   },
 });
